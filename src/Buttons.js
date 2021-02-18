8b33041cd6f98885320545aa527664d606488095
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2021-02-17 22:09:28 -0500

    removed underline on a tag

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-// adapted from react-github-btn
-
 const Container = styled.div`
   margin: 0.5em;
 
@@ -15,6 +13,7 @@ const Container = styled.div`
     border-radius: 5px;
     background-image: linear-gradient(180deg, #fafbfc, #eff3f6 90%);
     font-size: 20px;
+    text-decoration: none;
   }
 
   .btn:hover {
@@ -43,12 +42,12 @@ const FlexBox = styled.span`
   align-items: center;
 `;
 
-export const GithubButton = ({ url, textContent }) => {
+export const GithubButton = ({ href, children }) => {
   return (
     <Container>
       <a
         className="btn"
-        href={url}
+        href={href}
         rel="noopener noreferrer"
         target="_blank"
         aria-label="Follow @meerkat-citronella on GitHub"
@@ -67,19 +66,19 @@ export const GithubButton = ({ url, textContent }) => {
               d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
             ></path>
           </svg>
-          <span>{textContent}</span>
+          <span>{children}</span>
         </FlexBox>
       </a>
     </Container>
   );
 };
 
-export const DevpostButton = ({ url, textContent }) => {
+export const DevpostButton = ({ href, children }) => {
   return (
     <Container>
       <a
         className="btn"
-        href={url}
+        href={href}
         rel="noopener noreferrer"
         target="_blank"
         aria-label="Follow @meerkat-citronella on GitHub"
@@ -112,7 +111,7 @@ export const DevpostButton = ({ url, textContent }) => {
               d="M 16.101562 8.449219 L 10.699219 8.449219 L 10.699219 23.601562 L 16.101562 23.601562 C 20.199219 23.601562 23.550781 20.199219 23.550781 16.050781 C 23.550781 11.898438 20.25 8.449219 16.101562 8.449219 Z M 16 20.550781 L 13.75 20.550781 L 13.75 11.449219 L 16 11.449219 C 18.5 11.449219 20.550781 13.5 20.550781 16 C 20.550781 18.5 18.5 20.550781 16 20.550781 Z M 16 20.550781 "
             />
           </svg>
-          <span>{textContent}</span>
+          <span>{children}</span>
         </FlexBox>
       </a>
     </Container>
